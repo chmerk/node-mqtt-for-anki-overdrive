2a8a6eb21b68cefd998985656cea19598c7bd0fe
IOT-SFDE <iot-sfde@IOT-SFDEs-Mac-mini.local>
2018-03-23 12:59:25 +0100

    adjustments in package.json (update of noble version, request added)

---
+++ logger.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function () {
+    //work in progress...
+    return {
+        "loggero" : function () {
+            if (loggerMode == 'extended') {
+                console.log("Joooo!");
+            } else {
+                console.log("No");
+            }
+        },
+    };
+};
